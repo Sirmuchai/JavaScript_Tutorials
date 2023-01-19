7bccda873e9caf728279aa1dde380f5af0fab533
Sirmuchai <sirmemu@gmail.com>
2023-01-19 17:05:51 +0300

    While Loops

--- Week_1/loop.js
+++ Week_1/loop.js
@@ -20,3 +20,19 @@ names.forEach(name => {
 for (const name of names) {
     console.log(name);    
 }
+
+// Using While loop
+
+let index = 0;
+
+while (index < names.length){
+    console.log(names[index])
+    index++;
+}
+
+// Do while Loop
+
+do {
+    console.log(names[index])
+    index ++;
+}while(index<names.length)
